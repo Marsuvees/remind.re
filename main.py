4a4de836be963349abe8e2c0431eac43649e108c
Jed <tolujed@gmail.com>
2023-10-27 16:29:16 +0100

    to be continued at home...

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, render_template, redirect
 from database import ReminderDetails, session as sess
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import csv 
+
 app = Flask(__name__)
 
 database_file = './remind.re/database.csv'
@@ -15,6 +17,12 @@ def home():
 @app.route("/<string:page_name>")
 def page(page_name):
     return render_template(page_name)
+
+def write_to_csv(data):
+    field_names = ["tenant_name","tenant_phone_number","rent_amount","rent_period_in_months","date_of_initial_payment","rent_expiry_date"]
+    with open('database.csv', '+a') as database:  # You will need 'wb' mode in Python 2.x
+        writer = csv.DictWriter(database, fieldnames=field_names)
+        writer.writerow(data)
 
 # Define the route to process the form
 @app.route('/process_form', methods=['POST', 'GET'])
@@ -31,9 +39,21 @@ def create_reminder():
         # Calculate rent expiry date
         rent_expiry_date = date_of_initial_payment + relativedelta(months=int(rent_period_in_months))  
         new_reminder.rent_expiry_date = rent_expiry_date
-        
+
         sess.add(new_reminder)
         sess.commit()
+
+        # Write to csv
+        info = dict()
+        info['tenant_name'] =  request.form['tenant_name']
+        info['tenant_phone_number'] = request.form['tenant_phone_number']
+        info['rent_amount'] = request.form['rent_amount']
+        info['date_of_initial_payment'] = date_of_initial_payment
+        info['rent_period_in_months'] = request.form['rent_period_in_months']
+        info['rent_expiry_date'] = rent_expiry_date
+        print(info)
+        write_to_csv(info)
+
         return redirect("/thankyou.html")
     else:
         return 'Oops something went wrong!!!'
--- notifier.py
+++ notifier.py
@@ -5,29 +5,36 @@ from pathlib import Path
 from database import ReminderDetails, session as sess
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import csv
 
-# def e_mail(mail_dict, days_left):
-#     html = Template(Path('./due_email.html').read_text())
-#     email = EmailMessage()
-#     email['from'] = 'NoReply'
-#     email['to'] = email_address
-#     email['subject'] = 'Reminder Test!!!'
+def e_mail_with_dict(mail_dict):
+    tenant_name = mail_dict['tenant_name']
+    tenant_phone_number = mail_dict['tenant_phone_number']
+    rent_amount = mail_dict['rent_amount']
+    initial_date_of_payment = mail_dict['date_of_initial_payment']
+    rent_expiry_date = mail_dict['rent_expiry_date']
 
-#     email.set_content(html.substitute(name = tenant_name,
-#                                       tenant_phonenumber = tenant_phone_number,
-#                                       init_payment_date = initial_date_of_payment,
-#                                       rent_expirty_date = rent_expiry_date,
-#                                       rent_amount =  rent_amount),
-#                         'html')
+    html = Template(Path('./due_email.html').read_text())
+    email = EmailMessage()
+    email['from'] = 'NoReply'
+    email['to'] = 'tolujed@gmail.com'
+    email['subject'] = 'Reminder Test!!!'
 
-#     with smtplib.SMTP(host='smtp.gmail.com', port = 587) as smtp:
-#         smtp.ehlo()
-#         smtp.starttls()
-#         smtp.login('kiingjay77@gmail.com', 'ioiwirwvfyiefwgf')
-#         smtp.send_message(email)
-#         print ('All done!!!')
+    email.set_content(html.substitute(name = tenant_name,
+                                      tenant_phonenumber = tenant_phone_number,
+                                      init_payment_date = initial_date_of_payment,
+                                      rent_expirty_date = rent_expiry_date,
+                                      rent_amount =  rent_amount),
+                        'html')
 
-def e_mail(tenant_name, tenant_phone_number, initial_date_of_payment, rent_expiry_date, rent_amount):
+    with smtplib.SMTP(host='smtp.gmail.com', port = 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login('kiingjay77@gmail.com', 'ioiwirwvfyiefwgf')
+        smtp.send_message(email)
+        print ('All done!!!')
+
+def e_mail_database(tenant_name, tenant_phone_number, initial_date_of_payment, rent_expiry_date, rent_amount):
     html = Template(Path('./email.html').read_text())
     email = EmailMessage()
     email['from'] = 'NoReply'
@@ -50,13 +57,27 @@ def e_mail(tenant_name, tenant_phone_number, initial_date_of_payment, rent_expir
 
 # Utility function to check and send reminders
 def check_reminder():
-    reminders = sess.query(ReminderDetails).all()
-    for reminder in reminders:
-        if (reminder.rent_expiry_date - datetime.today().date()).days <= 30:
-            e_mail(reminder.tenant_name, reminder.tenant_phone_number, reminder.date_of_initial_payment, reminder.rent_expiry_date, reminder.rent_amount)
-            
+    try:
+        reminders = sess.query(ReminderDetails).all()
+        for reminder in reminders:
+            if (reminder.rent_expiry_date - datetime.today().date()).days <= 30:
+                e_mail_with_dict(reminder.tenant_name, reminder.tenant_phone_number, reminder.date_of_initial_payment, reminder.rent_expiry_date, reminder.rent_amount)
+    except:
+        # Open the CSV file for reading
+        with open('database.csv', mode='r') as file:
+            # Create a CSV reader using DictReader
+            csv_reader = csv.DictReader('database.csv')
 
-# Remove outdated reminders from datatbase file
+            # Loop through each row in the CSV file
+            for row in csv_reader:
+                if (datetime.strptime(row['rent_expiry_date'], "%Y-%m-%d").date() - datetime.today().date()).days <= 30:
+                    e_mail_with_dict(row)
+
+                # Each row is already a dictionary
+
+    
+
+# # Remove outdated reminders from datatbase file
 # def delete_data():
 #     today_date = datetime.datetime.today().date()
 #     dataframe = pd.read_csv(data_file, index_col=False)
